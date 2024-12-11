719b9959e2c5933121f0e9b495bd07b87ce42384
Sangwon Choi <sangwon.choi@nokia.com>
2024-12-11 09:54:15 +0000

    Add upload example

--- app/src/main/java/com/swc/sampleapp_mvvm/network/ApiService.kt
+++ app/src/main/java/com/swc/sampleapp_mvvm/network/ApiService.kt
@@ -2,7 +2,9 @@ package com.swc.sampleapp_mvvm.network
 
 import com.swc.sampleapp_mvvm.model.remote.PostResponse
 import com.swc.sampleapp_mvvm.model.remote.WeatherResponse
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface WeatherApiService {
@@ -17,4 +19,8 @@ interface PostApiService {
     @GET("posts")
     suspend fun getPosts(
     ): List<PostResponse>
+
+    // POST request to create a new post
+    @POST("posts")
+    suspend fun createPost(@Body newPost: PostResponse): PostResponse
 }
