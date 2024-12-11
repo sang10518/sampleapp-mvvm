79a67dc3f2cff4270358c705c8b48dc222255f1c
Sangwon Choi <sangwon.choi@nokia.com>
2024-12-11 09:53:38 +0000

    Set glide cache

--- app/src/main/java/com/swc/sampleapp_mvvm/GlideModule.kt
+++ app/src/main/java/com/swc/sampleapp_mvvm/GlideModule.kt
@@ -1,13 +1,12 @@
 package com.swc.sampleapp_mvvm
 
-import android.app.Application
 import android.content.Context
-import com.bumptech.glide.Glide
 import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory
+import com.bumptech.glide.load.engine.cache.LruResourceCache
 import com.bumptech.glide.module.AppGlideModule
-import dagger.hilt.android.HiltAndroidApp
+
 
 @GlideModule
 class MyGlideModule : AppGlideModule() {
@@ -16,5 +15,8 @@ class MyGlideModule : AppGlideModule() {
         builder.setDiskCache(
             InternalCacheDiskCacheFactory(context, "cacheFolderName", diskCacheSizeBytes)
         )
+
+        val memoryCacheSizeBytes = 20 * 1024 * 1024 // 20 MB
+        builder.setMemoryCache(LruResourceCache(memoryCacheSizeBytes.toLong()))
     }
 }
